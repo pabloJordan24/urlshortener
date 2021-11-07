8bda1ddb95f0e0d217366d985ad0699975fc66f4
pabloJordan24 <79514500+pabloJordan24@users.noreply.github.com>
2021-11-07 17:47:22 +0100

    Add files via upload

--- app/src/main/kotlin/es/unizar/urlshortener/Configuration.kt
+++ app/src/main/kotlin/es/unizar/urlshortener/Configuration.kt
@@ -3,6 +3,7 @@ package es.unizar.urlshortener
 import es.unizar.urlshortener.core.usecases.CreateShortUrlUseCaseImpl
 import es.unizar.urlshortener.core.usecases.LogClickUseCaseImpl
 import es.unizar.urlshortener.core.usecases.RedirectUseCaseImpl
+import es.unizar.urlshortener.core.usecases.InfoShortUrlUseCaseImpl
 import es.unizar.urlshortener.infrastructure.delivery.HashServiceImpl
 import es.unizar.urlshortener.infrastructure.delivery.ValidatorServiceImpl
 import es.unizar.urlshortener.infrastructure.repositories.ClickEntityRepository
@@ -43,4 +44,7 @@ class ApplicationConfiguration(
 
     @Bean
     fun createShortUrlUseCase() = CreateShortUrlUseCaseImpl(shortUrlRepositoryService(), validatorService(), hashService())
+
+    @Bean
+    fun infoShortUrlUseCase() = InfoShortUrlUseCaseImpl(shortUrlRepositoryService(), clickRepositoryService())
 }
--- app/src/main/resources/static/js/app.js
+++ app/src/main/resources/static/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(
-    function() {
+    function () {
         $("#shortener").submit(
             function(event) {
                 event.preventDefault();
@@ -20,5 +20,31 @@ $(document).ready(
                             "<div class='alert alert-danger lead'>ERROR</div>");
                     }
                 });
+            }
+        );
+  
+        $("#infoShortUrl").submit(
+        function(event) {
+            event.preventDefault();
+            //alert($(this).serialize().split("tiny-").pop())
+            $.ajax({
+                type : "GET",
+                url : "/info/"+$(this).serialize().split("tiny-").pop(),
+                success : function(response) {
+                    $("#resultInfo").html(
+                        "<div class='alert alert-info lead'>"
+                        + "<p>" + "Total number of clicks: " + response.numClicks +"</p>"
+                        + "<p>" + "Date of creation: " + response.creationDate +"</p>"
+                        + "<p>" + "Target URL: " + response.uriDestino +"</p>"
+                        + "</div>"
+                    );
+                },
+                error : function() {
+                     $("#resultInfo").html(
+                        "<div class='alert alert-danger lead'>ERROR</div>");
+                }
             });
-    });
+        });
+    }
+    
+);
