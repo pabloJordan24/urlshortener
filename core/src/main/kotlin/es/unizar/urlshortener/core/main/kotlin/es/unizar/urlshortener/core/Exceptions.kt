02e305c2982546714f0367f45964f88789aa94ed
pabloJordan24 <79514500+pabloJordan24@users.noreply.github.com>
2021-11-08 21:09:10 +0100

    Add files via upload

---
+++ core/src/main/kotlin/es/unizar/urlshortener/core/main/kotlin/es/unizar/urlshortener/core/Exceptions.kt
@@ -0,0 +1,7 @@
+package es.unizar.urlshortener.core
+
+class InvalidUrlException(val url: String) : Exception("[$url] does not follow a supported schema")
+
+class RedirectionNotFound(val key: String) : Exception("[$key] is not known")
+
+class NotReachableException(val url: String, val typeError: String) : Exception("[$url]"+"$typeError")
