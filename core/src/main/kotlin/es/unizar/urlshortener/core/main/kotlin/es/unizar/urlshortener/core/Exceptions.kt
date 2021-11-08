9ee7f6f6e1fc21555d5a80b8eae21ecffa310c59
pabloJordan24 <79514500+pabloJordan24@users.noreply.github.com>
2021-11-08 21:11:52 +0100

    Delete core/src/main/kotlin/es/unizar/urlshortener/core/main/kotlin/es/unizar/urlshortener/core directory

--- core/src/main/kotlin/es/unizar/urlshortener/core/main/kotlin/es/unizar/urlshortener/core/Exceptions.kt
+++
@@ -1,7 +0,0 @@
-package es.unizar.urlshortener.core
-
-class InvalidUrlException(val url: String) : Exception("[$url] does not follow a supported schema")
-
-class RedirectionNotFound(val key: String) : Exception("[$key] is not known")
-
-class NotReachableException(val url: String, val typeError: String) : Exception("[$url]"+"$typeError")
