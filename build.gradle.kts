12680e1305b8e474065faeb020d55e3521f47dcf
pabloJordan24 <79514500+pabloJordan24@users.noreply.github.com>
2021-12-13 18:38:44 +0100

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,10 +28,17 @@ subprojects {
     }
     dependencies {
         "implementation"("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        "implementation"("io.github.microutils:kotlin-logging-jvm:2.0.10")
     }
 }
 
-project(":core") { }
+project(":core") { 
+    apply(plugin="org.springframework.boot")
+    apply(plugin="io.spring.dependency-management")
+    dependencies {
+        "implementation" ("org.springframework.boot:spring-boot-starter")
+    }
+}
 
 project(":repositories") {
     apply(plugin = "org.springframework.boot")
@@ -40,6 +47,7 @@ project(":repositories") {
     dependencies {
         "implementation"(project(":core"))
         "implementation"("org.springframework.boot:spring-boot-starter-data-jpa")
+        "implementation"("io.github.microutils:kotlin-logging-jvm:2.0.10")
     }
     tasks.getByName<BootJar>("bootJar") {
         enabled = false
@@ -57,6 +65,8 @@ project(":delivery") {
         "implementation"("com.fasterxml.jackson.module:jackson-module-kotlin")
         "implementation"("commons-validator:commons-validator:1.6")
         "implementation"("com.google.guava:guava:23.0")
+        "implementation"("io.github.g0dkar:qrcode-kotlin:1.1.0")
+        "implementation"("io.github.microutils:kotlin-logging-jvm:2.0.10")
 
         "testImplementation"("org.springframework.boot:spring-boot-starter-test")
         "testImplementation"("org.mockito.kotlin:mockito-kotlin:3.2.0")
@@ -75,8 +85,9 @@ project(":app") {
         "implementation"(project(":delivery"))
         "implementation"(project(":repositories"))
         "implementation"("org.springframework.boot:spring-boot-starter")
-        "implementation"( "org.webjars:bootstrap:3.3.5")
+        "implementation"("org.webjars:bootstrap:3.3.5")
         "implementation"("org.webjars:jquery:2.1.4")
+        "implementation"("io.github.microutils:kotlin-logging-jvm:2.0.10")
 
         "runtimeOnly"("org.hsqldb:hsqldb")
 
